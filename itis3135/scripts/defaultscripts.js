5a205ee78d2be195f79da61c2b9516aa52e2d55d
jfarr3UNCC <jfarr3@uncc.edu>
2023-09-28 21:11:33 -0400

    Update defaultscripts.js

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -1,17 +1,14 @@
 function updateDateTime() {
-    // Get the current date and time
-    const now = new Date();
 
-    // Get the day of the week
+    const d = new Date();
+
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const dayOfWeek = daysOfWeek[now.getDay()];
+    const dayOfWeek = daysOfWeek[d.getDay()];
 
-    // Get the date in the format "d Month, YYYY"
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    const formattedDate = now.toLocaleDateString(undefined, options);
+    const formattedDate = d.toLocaleDateString(undefined, options);
 
-    // Get the time in the format "h:mm a"
-    const formattedTime = now.toLocaleTimeString(undefined, { hour: 'numeric', minute: '2-digit' });
+    const formattedTime = d.toLocaleTimeString(undefined, { hour: 'numeric', minute: '2-digit' });
 
     const displayString = `Today is ${formattedTime} on ${dayOfWeek}, ${formattedDate}`;
 
