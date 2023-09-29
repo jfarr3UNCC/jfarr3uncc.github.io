5f393d30a08410a34bf9f92a84194fb6935a4b59
jfarr3UNCC <jfarr3@uncc.edu>
2023-09-28 22:16:44 -0400

    greeting

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -8,11 +8,26 @@ function updateDateTime() {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     const formattedDate = d.toLocaleDateString(undefined, options);
 
-    const formattedTime = d.toLocaleTimeString(undefined, { hour: 'numeric', minute: '2-digit' });
+    const formattedTime = d.toLocaleTimeString(undefined, { hour: 'numeric', minute: '2-digit', second: '2-digit'});
 
     const displayString = `Today is ${formattedTime} on ${dayOfWeek}, ${formattedDate}`;
 
     document.getElementById("datetime").textContent = displayString;
 }
 
+document.getElementById('greetingForm').addEventListener('submit', function(event) {
+
+    event.preventDefault();
+
+    const userName = document.getElementById('name').value;
+    const userMood = document.getElementById('mood').value;
+
+    document.getElementById('userName').innerHTML = userName;
+    document.getElementById('userMood').innerHTML = userMood;
+
+    document.getElementById('greetingMessage').style.display = 'block';
+
+});
+
 updateDateTime();
+setInterval(updateDateTime, 1000);
