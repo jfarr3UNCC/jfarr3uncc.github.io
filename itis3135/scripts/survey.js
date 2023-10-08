585d7c432fb7702f6e2b521c3f14f0bfc085a872
jfarr3UNCC <jfarr3@uncc.edu>
2023-10-08 14:48:42 -0400

    fixed image loading

--- itis3135/scripts/survey.js
+++ itis3135/scripts/survey.js
@@ -1,16 +1,18 @@
 document.getElementById('newForm').addEventListener('submit', function(event){
 
-    event.preventDefault;
-
     var userName = document.getElementById('name').value;
     var userMascot = document.getElementById('mascot').value;
-    var userCaption = document.getElementById('caption').value;
+    var userCaption = document.getElementById('caption').value;0-
     var userPersonalBackground = document.getElementById('personalBackground').value;
     var userProfessionalBackground = document.getElementById('professionalBackground').value;
     var userAcademicBackground = document.getElementById('academicBackground').value;
     var userWebBackground = document.getElementById('webBackground').value;
     var userPlatform = document.getElementById('platform').value
     var userFunny = document.getElementById('funny').value;
+    var userImage = document.getElementById('image').files[0];
+
+    const imageURL = URL.createObjectURL(userImage);
+    var text = "<img src=\"" + imageURL + "\">";
 
     document.getElementById('userName').innerHTML = userName;
     document.getElementById('userMascot').innerHTML = userMascot;
@@ -21,7 +23,9 @@ document.getElementById('newForm').addEventListener('submit', function(event){
     document.getElementById('userWebBackground').innerHTML = userWebBackground;
     document.getElementById('userPlatform').innerHTML = userPlatform ;
     document.getElementById('userFunny').innerHTML = userFunny;
+    document.getElementById('loadImage').innerHTML = text;
     
 
     document.getElementById('userIntro').style.display = 'block';
 });
+
