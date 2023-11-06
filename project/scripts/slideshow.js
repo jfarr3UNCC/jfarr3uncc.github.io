fe1eeef357dd9a59d803e321ac2dbb1369a7f917
jfarr3UNCC <jfarr3@uncc.edu>
2023-11-05 21:39:50 -0500

    hgjghjg

---
+++ project/scripts/slideshow.js
@@ -0,0 +1,31 @@
+let slideIndex1 = 0;
+let slideIndex2 = 0;
+
+
+showSlides();
+
+function showSlides() {
+    let i;
+    let e;
+
+    let slides1 = document.getElementsByClassName("slides");
+    let slides2 = document.getElementsByClassName("slides2");
+
+    for (i = 0; i < slides1.length; i++) {
+      slides1[i].style.display = "none";  
+    }
+    slideIndex1++;
+    if (slideIndex1 > slides1.length) {slideIndex1 = 1}    
+    slides1[slideIndex1-1].style.display = "block";  
+
+    for (e = 0; e < slides2.length; e++) {
+      slides2[e].style.display = "none";  
+    }
+    slideIndex2++;
+    if (slideIndex2 > slides2.length) {slideIndex2 = 1}    
+    slides2[slideIndex2-1].style.display = "block";  
+
+
+
+    setTimeout(showSlides, 5000); 
+}
---
+++ project/scripts/slideshowCommission.js
@@ -0,0 +1,18 @@
+let slideIndex1 = 0;
+
+showSlides();
+
+function showSlides() {
+    let i;
+
+    let slides1 = document.getElementsByClassName("slides");
+
+    for (i = 0; i < slides1.length; i++) {
+      slides1[i].style.display = "none";  
+    }
+    slideIndex1++;
+    if (slideIndex1 > slides1.length) {slideIndex1 = 1}    
+    slides1[slideIndex1-1].style.display = "block";  
+
+    setTimeout(showSlides, 5000); 
+}
